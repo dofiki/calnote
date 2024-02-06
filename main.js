18f6adfb76ef3f7c63643cd0a59cbbcf16e6db54
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-02-06 14:31:08 +0545

    styles added

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-let first7 = document.querySelectorAll(".f7 div");
-let first7notes = document.querySelectorAll(".nf7 div");
-let calendarSection = document.querySelector('.calendarSection')
-first7.forEach((element,index)=>{
-    element.addEventListener('click',()=>{
-        //to remove previous note
-        first7notes.forEach((nelement)=>{
-            nelement.classList.remove('active')
-        })
-        first7notes[index].classList.add('active');
-        
-    })
-})
+let dates = document.querySelectorAll(".dates div");
+let notes = document.querySelectorAll(".notes .note");
 
-/*
-
-note tab functionality
-save button
-local storage
-
-
-*/
+dates.forEach((date, index) => {
+    date.addEventListener('click', () => {
+        notes.forEach((note, noteIndex) => {
+            if (noteIndex === index) {
+                note.classList.add('active');
+            } else {
+                note.classList.remove('active');
+            }
+        });
+    });
+});
