68f35ca67be5f64ce27e7487b043d368dfa5fd8e
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-02-07 22:03:29 +0545

    added local storage feature

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let dates = document.querySelectorAll(".dates div");
 let notes = document.querySelectorAll(".notes .note");
+let noteContent = document.querySelectorAll(".noteContent");
+let saveBtn = document.querySelectorAll('.noteMenu img');
+
+function showContent() {
+    noteContent.forEach((content, index) => {
+        let storedNote = localStorage.getItem(`note_${index}`);
+        if (storedNote) {
+            content.innerHTML = storedNote;
+        }
+    });
+}
+
+showContent();
 
 dates.forEach((date, index) => {
     date.addEventListener('click', () => {
@@ -10,5 +23,12 @@ dates.forEach((date, index) => {
                 note.classList.remove('active');
             }
         });
+    });
+});
+
+saveBtn.forEach((btn, btnIndex) => {
+    btn.addEventListener('click', () => {
+        let content = noteContent[btnIndex].innerHTML;
+        localStorage.setItem(`note_${btnIndex}`, content);
     });
 });
